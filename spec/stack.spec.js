03cb4788b944c7e11b196de3f6e5e0eb5ef9d0d9
Bernard Lee <bernardlee82@gmail.com>
2017-03-24 22:54:45 -0400

    Update to use Standard JavaScript Style
    Run "grunt make-standard".

--- spec/stack.spec.js
+++ spec/stack.spec.js
@@ -1,81 +1,81 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
+const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 
 chai.use(chaiAsPromised)
 
-const expect = chai.expect;
+const expect = chai.expect
 
-const Stack = require('../lib/stack');
+const Stack = require('../lib/stack')
 
 describe('Stack', function () {
   it('creates an empty stack', function () {
-    const stack = new Stack();
-    const empty = {};
+    const stack = new Stack()
+    const empty = {}
 
-    expect(stack).to.be.an.instanceof(Stack);
-    expect(stack._storage).to.deep.equal(empty);
-  });
+    expect(stack).to.be.an.instanceof(Stack)
+    expect(stack._storage).to.deep.equal(empty)
+  })
 
   xit('creates a stack containing the arguments', function () {
-    const stack = new Stack('a', 'b', 'c');
+    const stack = new Stack('a', 'b', 'c')
     const storage = {
       0: 'a',
       1: 'b',
-      2: 'c',
-    };
+      2: 'c'
+    }
 
-    expect(stack._storage).to.deep.equal(storage);
-  });
-});
+    expect(stack._storage).to.deep.equal(storage)
+  })
+})
 
 describe('Stack.prototype.push()', function () {
   it('adds new values to the stack', function () {
-    const stack = new Stack();
+    const stack = new Stack()
     const pushed = {
-      foo: 'bar',
-    };
+      foo: 'bar'
+    }
 
-    stack.push(pushed);
+    stack.push(pushed)
 
     expect(stack._storage).to.deep.equal({
-      0: pushed,
-    });
-  });
+      0: pushed
+    })
+  })
 
   xit('returns the stack itself', function () {
-    const stack = new Stack();
+    const stack = new Stack()
     const pushed = {
-      foo: 'bar',
-    };
+      foo: 'bar'
+    }
 
-    expect(stack.push(pushed)).to.equal(stack);
-  });
-});
+    expect(stack.push(pushed)).to.equal(stack)
+  })
+})
 
 describe('Stack.prototype.pop()', function () {
   it('removes the most recently added value from the stack', function () {
-    const stack = new Stack();
-    const empty = {};
+    const stack = new Stack()
+    const empty = {}
 
-    stack.push(1);
-    stack.pop();
+    stack.push(1)
+    stack.pop()
 
-    expect(stack._storage).to.deep.equal(empty);
-  });
+    expect(stack._storage).to.deep.equal(empty)
+  })
 
   it('returns the value removed from the stack', function () {
-    const stack = new Stack();
+    const stack = new Stack()
 
-    stack.push(1);
+    stack.push(1)
 
-    expect(stack.pop()).to.equal(1);
-  });
+    expect(stack.pop()).to.equal(1)
+  })
 
   it('returns undefined when called on an empty stack', function () {
-    const stack = new Stack();
+    const stack = new Stack()
 
-    expect(stack.pop()).to.be.undefined;
-  });
-});
+    expect(stack.pop()).to.be.undefined
+  })
+})
